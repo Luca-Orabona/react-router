4cdaf66097f60c8fd468fea0963eaf6f5dfc89b1
Luca-Orabona <lucaorabona94@gmail.com>
2025-06-12 16:37:26 +0200

    feat: chiamata API della lista dei post

--- src/pages/ListaPost.jsx
+++ src/pages/ListaPost.jsx
@@ -1,15 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const ListaPost = () => {
+    const [dataPost, setDtaPost] = useState([]);
+    const [spinner, setSpinner] = useState(true);
+
+    useEffect(() => {
+        axios.get("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts")
+            .then((resp) => {
+                setDtaPost(resp.data);
+            })
+            .finally(() => {
+                setSpinner(false); 
+            });
+    }, []);
 
     return (
-        
         <div className="container">
-            <h1>Lista post</h1>
+            <h1 className="text-center mb-4">Lista post</h1>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, impedit? Dolorum eum natus perspiciatis magni at pariatur debitis explicabo quis?</p>
+            {spinner ? (
+                
+                <div className="d-flex justify-content-center mb-3">
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            ) : (
+               
+                <div className="row row-cols-3 g-4">
+                    {dataPost.map((curPost, index) => (
+                        <div key={index} className="col">
+                            <div className="card h-100">
+                                <div className="card-body">
+                                    <h2>{`${curPost.id} - ${curPost.author}`}</h2>
+                                    <p>{curPost.body}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-
-    )
-
-}
+    );
+};
 
 export default ListaPost;
